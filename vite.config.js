b1cfc21aed30cf8153ea635f21183e66ce0124da
AntMee <69108724+AntMee@users.noreply.github.com>
2026-06-16 22:45:46 +0800

    chore: obfuscate frontend production assets

--- vite.config.js
+++ vite.config.js
@@ -6,12 +6,26 @@ export default defineConfig({
   build: {
     outDir: "dist",
     emptyOutDir: true,
-    minify: "esbuild",
+    minify: "terser",
     sourcemap: false,
     cssMinify: true,
     reportCompressedSize: false,
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+        passes: 2
+      },
+      format: {
+        comments: false
+      },
+      mangle: {
+        toplevel: true
+      }
+    },
     rollupOptions: {
       output: {
+        compact: true,
         entryFileNames: "assets/[hash].js",
         chunkFileNames: "assets/[hash].js",
         assetFileNames: "assets/[hash][extname]"
